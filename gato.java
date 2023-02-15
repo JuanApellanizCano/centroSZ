1e49048d9f467981dda2400923c4b4058512adb9
Alberto Saz <albertosaz@gmail.com>
2023-02-15 20:04:03 +0100

    Mis primeros pasos en GITHUB

--- gato.java
+++ gato.java
@@ -1,13 +1,8 @@
 public class gato{
 
     public static void main(String args[]){
-<<<<<<< HEAD
-        System.out.print("Pio");
-=======
         System.out.print("Miau");
-        ESTE ES EL CAMBIO
->>>>>>> d1a182f00b7ed40c2a1579abfc55902a1a3a5526
     }
-    }
+}
 
 // EL BARTO
