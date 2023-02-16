dce6d5bc1ee8d4c8be71f82a8ec07373cc211485
Jorge Esteban <esteban.gijor22@git.salesianos.edu>
2023-02-16 10:53:08 +0100

    Mis primeros pasos en GITHUB

--- perro.java
+++ perro.java
@@ -1,7 +1,17 @@
 
 public class perro{
 
-public static void main(String args[]){
-    System.out.print("Perro");
-}
+    public static void main(String args[]){
+        System.out.print("Perro");
+        Ladrar(grande);
+        Ladrar(pequeño);
+    }
+
+    public static void Ladrar(String tamañoraza){
+        if(tamañoraza == "grande")
+            System.out.print("Woof! Woof!");
+        if(tamañoraza == "pequeño")
+            System.out.print("Guau!");
+    }
+        
 }
