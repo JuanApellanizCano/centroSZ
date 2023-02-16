6072005e89e04577d1575d48d31401590b8b32c2
Ismael Bernad Tello <bernad.teism20@gmail.com>
2023-02-16 15:48:44 +0100

    Mis primeros pasos en GITHUB

--- perro.java
+++ perro.java
@@ -10,8 +10,11 @@ public class perro{
     public static void Ladrar(String tamañoraza){
         if(tamañoraza == "grande")
             System.out.print("Se que los gatos son mejores");
-        if(tamañoraza == "pequeño")
-            System.out.print("Los perros pequeños tambien se llaman ratas);
+        if(tamañoraza == "pequeño"){
+            System.out.print("Los perros pequeños tambien se llaman ratas");
+        //ESTE ES EL CAMBIO EN EL CÓDIGO:
+        System.out.print("Guau! Guau!");
+        }
     }
         
 }
